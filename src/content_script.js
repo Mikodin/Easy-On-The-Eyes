abd4b4087a14e8f7a690140a60c4335170ddf698
Michael Alicea <mfalicea58@gmail.com>
2016-10-19 20:05:54 -0400

    Succesfull injects an html string into the header

--- src/content_script.js
+++ src/content_script.js
@@ -3,11 +3,20 @@ var cssId = 'readable.css';  // you could encode the css path itself to generate
   if (!document.getElementById(cssId))
 {
   var head  = document.getElementsByTagName('head')[0];
+  var style = document.createElement('style');
+  style.innerHTML = 'body{margin:1em auto;' +
+    'max-width:40em;' +
+    'padding:0 .62em;' +
+    'font:1.2em/1.62em sans-serif;}' +
+    'h1,h2,h3{line-height:1.2em;}' +
+    '@media print{body{max-width:none}};' +
+    /*
   var link  = document.createElement('link');
   link.id   = cssId;
   link.rel  = 'stylesheet';
   link.type = 'text/css';
-  link.href = 'https://github.com/Rayne58/Easy-On-The-Eyes/blob/master/src/styles.css';
+  link.href = 'https://github.com/Rayne58/Easy-On-The-Eyes/blob/master/src/css/readable.css';
   link.media = 'all';
-  head.appendChild(link);
+  */
+    head.appendChild(style);
 }
